b44ed3c1c9c7d3b76e58e1c9f27299bb6f425d1d
Danilo Borges <danilo.cb@live.com>
2025-05-28 20:17:47 +0000

    add faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i],this.addEventListener('click', function(botao) {
@@ -12,7 +13,21 @@ document.addEventListener('DOMContentLoaded', function() {
         })
 
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i],this.addEventListener('click', abreOuFechaRespostas);
+        
+    }
+
 })
+
+function abreOuFechaRespostas(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
